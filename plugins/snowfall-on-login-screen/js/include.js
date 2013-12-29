7e0a1500a6de608b55dfdafd258af4a58453e84e
RainLoop Team <support@rainloop.net>
2013-12-30 01:24:30 +0400

    Disable snow animation for mobile devices

--- plugins/snowfall-on-login-screen/js/include.js
+++ plugins/snowfall-on-login-screen/js/include.js
@@ -2,8 +2,17 @@
 $(function () {
 	if (window.snowFall && !window.rl.settingsGet('Auth'))
 	{
-		window.snowFall.snow(document.getElementsByTagName('body'), {
-			shadow: true, round: true,  minSize: 2, maxSize: 5
-		});
+		var
+			sUserAgent = (navigator.userAgent || '').toLowerCase(),
+			bIsiOSDevice = -1 < sUserAgent.indexOf('iphone') || -1 < sUserAgent.indexOf('ipod') || -1 < sUserAgent.indexOf('ipad'),
+			bIsAndroidDevice = -1 < sUserAgent.indexOf('android')
+		;
+
+		if (!bIsiOSDevice && !bIsAndroidDevice)
+		{
+			window.snowFall.snow(document.getElementsByTagName('body'), {
+				shadow: true, round: true,  minSize: 2, maxSize: 5
+			});
+		}
 	}
 });
