dd41d30f9b31144e68cd696120d652e8a94be6f8
RainLoop Team <support@rainloop.net>
2015-03-09 23:47:57 +0400

    Added empty style file for ownCloud package

--- build/owncloud/rainloop-app/index.php
+++ build/owncloud/rainloop-app/index.php
@@ -13,6 +13,10 @@ OCP\User::checkLoggedIn();
 OCP\App::checkAppEnabled('rainloop');
 OCP\App::setActiveNavigationEntry('rainloop_index');
 
+// Load the empty file ../css/style.css, that's needed to allow theming of
+// the ownCloud header and navigation if rainloop is the active app.
+OCP\Util::addStyle('rainloop', 'style');
+
 include_once OC_App::getAppPath('rainloop').'/lib/RainLoopHelper.php';
 
 $sUrl = OC_RainLoop_Helper::normalizeUrl(OC_RainLoop_Helper::getAppUrl());
