cb006abbc1e7bf151c4790e448e735ed2f784222
RainLoop Team <support@rainloop.net>
2016-05-07 18:54:36 +0300

    Fix PclZip (PHP7 deprecated constructor)

--- rainloop/v/0.0.0/app/libraries/pclzip/pclzip.lib.php
+++ rainloop/v/0.0.0/app/libraries/pclzip/pclzip.lib.php
@@ -214,7 +214,7 @@
   //   Note that no real action is taken, if the archive does not exist it is not
   //   created. Use create() for that.
   // --------------------------------------------------------------------------------
-  function PclZip($p_zipname)
+  function __construct($p_zipname)
   {
 
     // ----- Tests the zlib
