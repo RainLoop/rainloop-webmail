5c2ea2ccda5e9c5ec31c5b6df7710c502d5eb0ff
RainLoop Team <support@rainloop.net>
2016-07-01 01:58:09 +0300

    Check test's dump

--- tests/Common/NpmTest.php
+++ tests/Common/NpmTest.php
@@ -7,7 +7,9 @@ class NpmTest extends \PHPUnit_Framework_TestCase
 	public function testJsValidate()
 	{
 		$out = array();
-		exec('gulp js:validate', $out);
+		exec('gulp js:eslint', $out);
+
+		var_dump($out);
 
 		$this->assertTrue(0 < \count($out));
 		$this->assertTrue(false === \strpos(\implode('|', $out), 'problem'));
