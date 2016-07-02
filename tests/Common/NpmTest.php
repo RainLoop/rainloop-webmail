49a4131628711d05bc8f9753bb4a1757683108ad
RainLoop Team <support@rainloop.net>
2016-07-02 13:50:03 +0300

    Improve test config

--- tests/Common/NpmTest.php
+++ tests/Common/NpmTest.php
@@ -7,9 +7,12 @@ class NpmTest extends \PHPUnit_Framework_TestCase
 	public function testJsValidate()
 	{
 		$out = array();
-		exec('gulp js:validate', $out);
 
-		$this->assertTrue(0 < \count($out));
+//		exec('gulp js:validate', $out);
+//		$this->assertTrue(0 < \count($out));
+
+		exec('eslint -c .eslintrc.js dev/*', $out);
+		$this->assertTrue(\is_array($out));
 
 		$noProblem = false === \strpos(\implode('|', $out), 'problem');
 		if (!$noProblem)
