7ffa5af91f05f72aaec1217b833b99d16cf3aa42
RainLoop Team <support@rainloop.net>
2016-09-13 20:25:39 +0300

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -316,18 +316,18 @@ gulp.task('css:social', function() {
 });
 
 gulp.task('css:main:min', ['css:main'], function() {
-	var cleanCSS = require('gulp-clean-css');
+	var csso = require('gulp-csso');
 	return gulp.src(cfg.paths.staticCSS + cfg.paths.css.main.name)
-		.pipe(cleanCSS())
+		.pipe(csso())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(eol('\n', true))
 		.pipe(gulp.dest(cfg.paths.staticCSS));
 });
 
 gulp.task('css:social:min', ['css:social'], function() {
-	var cleanCSS = require('gulp-clean-css');
+	var csso = require('gulp-csso');
 	return gulp.src(cfg.paths.staticCSS + cfg.paths.css.social.name)
-		.pipe(cleanCSS())
+		.pipe(csso())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(eol('\n', true))
 		.pipe(gulp.dest(cfg.paths.staticCSS));
