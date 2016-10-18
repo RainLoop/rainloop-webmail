52018b40e8089294f0e28aa2130344edfa6c35f6
RainLoop Team <support@rainloop.net>
2016-10-18 22:49:54 +0300

    Add login background animation

--- dev/Screen/User/Login.js
+++ dev/Screen/User/Login.js
@@ -1,4 +1,9 @@
 
+import $ from '$';
+import _ from '_';
+
+import {$win} from 'Common/Globals';
+
 import {AbstractScreen} from 'Knoin/AbstractScreen';
 
 import {LoginUserView} from 'View/User/Login';
@@ -13,6 +18,35 @@ class LoginUserScreen extends AbstractScreen
 
 	onShow() {
 		getApp().setWindowTitle('');
+		this.initFunBackgroud();
+	}
+
+	getHeightAndWidth() {
+		const
+			height = $win.height(),
+			width = $win.width();
+
+		return {height, width};
+	}
+
+	initFunBackgroud() {
+
+		const
+			$bg = $('#rl-bg'),
+			movementStrength = 25,
+			winHeight = $win.height(),
+			winWidth = $win.width(),
+			height = movementStrength / winHeight,
+			width = movementStrength / winWidth,
+			winHeightHalf = winHeight / 2,
+			winWidthHalf = winWidth / 2;
+
+		$('#rl-app').on('mousemove', _.throttle((e) => {
+			$bg.css({
+				top: height * (e.pageY - winHeightHalf) * -1 - movementStrength,
+				left: width * (e.pageX - winWidthHalf) * -1 - movementStrength
+			});
+		}, 1));
 	}
 }
 
