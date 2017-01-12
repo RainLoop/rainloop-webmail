f383380d5912a67f64c255b7db364b6962abb511
RainLoop Team <support@rainloop.net>
2017-01-13 01:50:30 +0300

    Update ckeditor

--- vendors/ckeditor/build-config.js
+++ vendors/ckeditor/build-config.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/9c8f8c7cb398fa35c352de7ead534613
+ * (2) http://ckeditor.com/builder/21fa3f877e42337855be1eec1e19053d
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/9c8f8c7cb398fa35c352de7ead534613
+ * (3) http://ckeditor.com/builder/download/21fa3f877e42337855be1eec1e19053d
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -25,7 +25,7 @@
  */
 
 var CKBUILDER_CONFIG = {
-	skin: 'icy_orange',
+	skin: 'moono-lisa',
 	preset: 'basic',
 	ignore: [
 		'.bender',
@@ -80,6 +80,7 @@ var CKBUILDER_CONFIG = {
 	languages : {
 		'af' : 1,
 		'ar' : 1,
+		'az' : 1,
 		'bg' : 1,
 		'bn' : 1,
 		'bs' : 1,
@@ -125,6 +126,7 @@ var CKBUILDER_CONFIG = {
 		'nb' : 1,
 		'nl' : 1,
 		'no' : 1,
+		'oc' : 1,
 		'pl' : 1,
 		'pt' : 1,
 		'pt-br' : 1,
