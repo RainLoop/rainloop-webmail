9a326377261bf444e996f0fcc162a24ff71137c2
BloodyAltair <me@bloody.pw>
2019-04-15 08:48:59 +0300

    Unsplash BG Updater plugin
    Signed-off-by: BloodyAltair <me@bloody.pw>

---
+++ plugins/unsplash-bg-updater/js/plugin.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2019 Denis Vadimov aka BloodyAltair
+ */
+(function () {
+    const updateRate = window.rl.pluginSettingsGet('unsplash-bg-updater', 'mUpdateRate');
+    if(updateRate < 10) {
+        console.log("[Settings] Update rate is mandatory positive integer!");
+        return -1;
+    }
+    // noinspection JSUnresolvedVariable,NodeModulesDependencies,ES6ModulesDependencies
+    window.unsplashBgUpdater.init(updateRate,"rl-bg","id",function () {
+        return new Promise(function(resolve, reject) {
+            window.rl.pluginRemoteRequest(function (sResult, oData) {
+                if(window.rl.Enums.StorageResultType.Success !== sResult) {
+                    reject(sResult)
+                }
+                // noinspection JSUnresolvedVariable
+                resolve(oData.Result);
+            }, 'getNewImage');
+        })
+    });
+    console.log("Unsplash background updater loaded!");
+}());
