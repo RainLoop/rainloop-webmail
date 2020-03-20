a1ce3f0e841f8d79aa8a89e1fa0fcfc85cfca1ff
djmaze <djmaze@djmaze.lan>
2020-03-20 16:01:23 +0100

    Terser strip comments

--- tasks/js.js
+++ tasks/js.js
@@ -64,7 +64,9 @@ const jsMin = () =>
 		.pipe(gulpif(config.source, sourcemaps.init({ loadMaps: true })))
 		.pipe(replace(/"rainloop\/v\/([^/]+)\/static\/js\/"/g, '"rainloop/v/$1/static/js/min/"'))
 		.pipe(rename({ suffix: '.min' }))
-		.pipe(terser())
+		.pipe(terser({
+			output: {comments: false}
+		}))
 		.pipe(eol('\n', true))
 		.pipe(gulpif(config.source, sourcemaps.write('./')))
 		.pipe(
