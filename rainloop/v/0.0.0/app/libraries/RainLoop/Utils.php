53fe66177a9b96178a9f844e829ad4e1b0eebcc8
djmaze <djmaze@djmaze.lan>
2020-08-19 10:43:10 +0200

    Bugfix: setcookie() samesite missing

--- rainloop/v/0.0.0/app/libraries/RainLoop/Utils.php
+++ rainloop/v/0.0.0/app/libraries/RainLoop/Utils.php
@@ -391,7 +391,14 @@ class Utils
 		}
 
 		static::$Cookies[$sName] = $sValue;
-		@\setcookie($sName, $sValue, $iExpire, $sPath, $sDomain, $bSecure, $bHttpOnly);
+		@\setcookie($sName, $sValue, array(
+			'expires' => $iExpire,
+			'path' => $sPath,
+			'domain' => $sDomain,
+			'secure' => $bSecure,
+			'httponly' => $bHttpOnly,
+			'samesite' => 'Strict'
+		));
 	}
 
 	public static function ClearCookie(string $sName)
