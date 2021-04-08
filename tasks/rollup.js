ff13ff688cc7996617eb1ae1952362bd9b095f6a
djmaze <djmaze@djmaze.lan>
2021-04-08 10:03:16 +0200

    Drop the use if Babel transcompiler

--- tasks/rollup.js
+++ tasks/rollup.js
@@ -1,6 +1,5 @@
 /* SnappyMail Webmail (c) SnappyMail Team | Licensed under AGPL 3 */
 const rollup2 = require('gulp-rollup-2');
-const babel = require('rollup-plugin-babel');
 const includePaths = require('rollup-plugin-includepaths');
 const externalGlobals = require('rollup-plugin-external-globals');
 const html = require('rollup-plugin-html');
@@ -13,30 +12,6 @@ let includePathOptions = {
 	extensions: ['.js', '.html']
 };
 
-let babelConfig = {
-	exclude: 'node_modules/**',
-	babelrc: false,
-	presets: [
-		[
-			'@babel/preset-env',
-			{
-				targets: {"chrome": "60"},
-				loose: true,
-				modules: false
-			}
-		]
-	],
-	plugins: [
-		[
-			'@babel/plugin-proposal-decorators',
-			{
-				legacy: true
-			}
-		],
-		'@babel/plugin-proposal-class-properties'
-	]
-};
-
 exports.rollupJS = (inputFile) =>
 	rollup2.src({
 		external: ['ko'],
@@ -45,7 +20,6 @@ exports.rollupJS = (inputFile) =>
 			{file: config.paths.staticJS + inputFile, format: 'iife'}
 		],
 		plugins: [
-			babel(babelConfig),
 			includePaths(includePathOptions),
 			externalGlobals({
 				ko: 'ko'
