a7ae087f1898c09aa6eb1cfe6dc7a4b2495f1812
djmaze <djmaze@djmaze.lan>
2021-05-01 10:02:40 +0200

    More HTML cleanups

--- dev/View/User/Settings/Menu.js
+++ dev/View/User/Settings/Menu.js
@@ -21,7 +21,7 @@ export class MenuSettingsUserView extends AbstractViewLeft {
 	onBuild(dom) {
 		dom.addEventListener('click', event =>
 			ThemeStore.isMobile()
-			&& event.target.closestWithin('.b-settings-menu a', dom)
+			&& event.target.closestWithin('.b-settins-left nav a', dom)
 			&& leftPanelDisabled(true)
 		);
 	}
