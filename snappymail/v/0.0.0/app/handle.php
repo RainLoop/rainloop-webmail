adb561943822b242a102a974e5d2ab09d3f0f0e3
djmaze <djmaze@djmaze.lan>
2021-08-09 15:08:42 +0200

    https://github.com/the-djmaze/snappymail/issues/99#issuecomment-894994640

--- snappymail/v/0.0.0/app/handle.php
+++ snappymail/v/0.0.0/app/handle.php
@@ -30,7 +30,12 @@ if (\class_exists('RainLoop\Api'))
 {
 	if (!\SnappyMail\HTTP\SecFetch::isEntering() && !\SnappyMail\HTTP\SecFetch::isSameOrigin()) {
 		\MailSo\Base\Http::StatusHeader(403);
-		exit('Disallowed Sec-Fetch-Site: ' . ($_SERVER['HTTP_SEC_FETCH_SITE'] ?? ''));
+		exit("Disallowed Sec-Fetch
+			Dest: " . ($_SERVER['HTTP_SEC_FETCH_DEST'] ?? '') . "
+			Mode: " . ($_SERVER['HTTP_SEC_FETCH_MODE'] ?? '') . "
+			Site: " . ($_SERVER['HTTP_SEC_FETCH_SITE'] ?? '') . "
+			User: " . (\SnappyMail\HTTP\SecFetch::user() ? 'true' : 'false')
+		);
 	}
 
 	\MailSo\Base\Loader::Init();
