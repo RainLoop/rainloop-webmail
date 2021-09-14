577c5f426e6850ea3f14a2ce6e4b1468b43939c7
djmaze <djmaze@djmaze.lan>
2021-09-14 16:24:32 +0200

    Bugfix: Argument 9 passed to MailSo\Mail\MailClient::MessageList() must be of the type int, string given https://github.com/the-djmaze/snappymail/issues/143#issuecomment-918543318

--- snappymail/v/0.0.0/app/libraries/RainLoop/Actions/Messages.php
+++ snappymail/v/0.0.0/app/libraries/RainLoop/Actions/Messages.php
@@ -23,7 +23,7 @@ trait Messages
 		$sSearch = '';
 		$iUidNext = 0;
 		$bUseThreads = false;
-		$iThreadUid = '';
+		$iThreadUid = 0;
 		$sSort = '';
 
 		$sRawKey = $this->GetActionParam('RawKey', '');
