d27e6fe97f0e455c837efd13a23c089b5bdc6e0c
djmaze <djmaze@djmaze.lan>
2021-10-21 20:39:11 +0200

    Added Czech language

---
+++ snappymail/v/0.0.0/app/localization/cs-CZ/relativetimeformat.js
@@ -0,0 +1,110 @@
+{
+	year: {
+		previous: "minulý rok",
+		current: "tento rok",
+		next: "příští rok",
+		past: {
+			one: "před {0} rokem",
+			many: "před {0} roku",
+			other: "před {0} lety"
+		},
+		future: {
+			one: "za {0} rok",
+			few: "za {0} roky",
+			many: "za {0} roku",
+			other: "za {0} let"
+		}
+	},
+	month: {
+		previous: "minulý měsíc",
+		current: "tento měsíc",
+		next: "příští měsíc",
+		past: {
+			one: "před {0} měsícem",
+			many: "před {0} měsíce",
+			other: "před {0} měsíci"
+		},
+		future: {
+			one: "za {0} měsíc",
+			few: "za {0} měsíce",
+			many: "za {0} měsíce",
+			other: "za {0} měsíců"
+		}
+	},
+	week: {
+		previous: "minulý týden",
+		current: "tento týden",
+		next: "příští týden",
+		past: {
+			one: "před {0} týdnem",
+			many: "před {0} týdne",
+			other: "před {0} týdny"
+		},
+		future: {
+			one: "za {0} týden",
+			few: "za {0} týdny",
+			many: "za {0} týdne",
+			other: "za {0} týdnů"
+		}
+	},
+	day: {
+		previous: "včera",
+		previous-2: "předevčírem",
+		current: "dnes",
+		next: "zítra",
+		next-2: "pozítří",
+		past: {
+			one: "před {0} dnem",
+			many: "před {0} dne",
+			other: "před {0} dny"
+		},
+		future: {
+			one: "za {0} den",
+			few: "za {0} dny",
+			many: "za {0} dne",
+			other: "za {0} dní"
+		}
+	},
+	hour: {
+		current: "tuto hodinu",
+		past: {
+			one: "před {0} hodinou",
+			many: "před {0} hodiny",
+			other: "před {0} hodinami"
+		},
+		future: {
+			one: "za {0} hodinu",
+			few: "za {0} hodiny",
+			many: "za {0} hodiny",
+			other: "za {0} hodin"
+		}
+	},
+	minute: {
+		current: "tuto minutu",
+		past: {
+			one: "před {0} minutou",
+			many: "před {0} minuty",
+			other: "před {0} minutami"
+		},
+		future: {
+			one: "za {0} minutu",
+			few: "za {0} minuty",
+			many: "za {0} minuty",
+			other: "za {0} minut"
+		}
+	},
+	second: {
+		current: "nyní",
+		past: {
+			one: "před {0} sekundou",
+			many: "před {0} sekundy",
+			other: "před {0} sekundami"
+		},
+		future: {
+			one: "za {0} sekundu",
+			few: "za {0} sekundy",
+			many: "za {0} sekundy",
+			other: "za {0} sekund"
+		}
+	}
+}
