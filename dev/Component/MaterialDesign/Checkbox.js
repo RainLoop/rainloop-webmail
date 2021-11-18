a92d5a0f4f33dd4fe9bc0c3f5d5037f24ce18b06
djmaze <djmaze@djmaze.lan>
2021-11-18 12:40:38 +0100

    Better (material) components design

--- dev/Component/MaterialDesign/Checkbox.js
+++ dev/Component/MaterialDesign/Checkbox.js
@@ -1,28 +1,3 @@
-import ko from 'ko';
 import { AbstractCheckbox } from 'Component/AbstractCheckbox';
 
-export class CheckboxMaterialDesignComponent extends AbstractCheckbox {
-	/**
-	 * @param {Object} params
-	 */
-	constructor(params) {
-		super(params);
-
-		this.animationBox = ko.observable(false).extend({ falseTimeout: 200 });
-		this.animationCheckmark = ko.observable(false).extend({ falseTimeout: 200 });
-
-		this.disposable.push(
-			this.value.subscribe(value => this.triggerAnimation(value), this)
-		);
-	}
-
-	triggerAnimation(box) {
-		if (box) {
-			this.animationBox(true);
-			setTimeout(()=>this.animationCheckmark(true), 200);
-		} else {
-			this.animationCheckmark(true);
-			setTimeout(()=>this.animationBox(true), 200);
-		}
-	}
-}
+export class CheckboxMaterialDesignComponent extends AbstractCheckbox {}
