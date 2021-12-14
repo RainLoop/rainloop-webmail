0905787f5f4def76fd731b6384879ae6aef8ac7d
djmaze <>
2021-12-14 21:34:47 +0100

    v2.9.6

--- integrations/virtualmin/snappymail.pl
+++ integrations/virtualmin/snappymail.pl
@@ -20,7 +20,7 @@ return "SnappyMail Webmail is a browser-based multilingual IMAP client with an a
 # script_snappymail_versions()
 sub script_snappymail_versions
 {
-return ( "2.9.5" );
+return ( "2.9.6" );
 }
 
 sub script_snappymail_version_desc
