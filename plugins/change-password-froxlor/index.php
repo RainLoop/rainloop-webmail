5cc738e2ba925fa0cfb927c27b2cc8bbfe42e6f9
the-djmaze <>
2022-05-30 13:12:01 +0200

    Resolve #422

---
+++ plugins/change-password-froxlor/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use \RainLoop\Exceptions\ClientException;
+
+class ChangePasswordPoppassdPlugin extends \RainLoop\Plugins\AbstractPlugin
+{
+	const
+		NAME     = 'Change Password Froxlor',
+		AUTHOR   = 'Euphonique',
+		VERSION  = '1.0',
+		RELEASE  = '2022-05-30',
+		REQUIRED = '2.15.3',
+		CATEGORY = 'Security',
+		DESCRIPTION = 'Extension to allow users to change their passwords through Froxlor';
+
+	public function Supported() : string
+	{
+		return 'Use Change Password plugin';
+	}
+}
