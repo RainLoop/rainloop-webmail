a8eee1dbcf025fbc8ae2990016966e9a7eb0b809
the-djmaze <>
2022-06-13 20:57:24 +0200

    Resolve #435

---
+++ snappymail/v/0.0.0/app/localization/vi-VN/relativetimeformat.js
@@ -0,0 +1,76 @@
+{
+	long: {
+		year: {
+			past: {
+				one: "{0} năm trước",
+				other: "{0} năm trước"
+			},
+			future: {
+				one: "trong {0} năm",
+				other: "trong {0} năm"
+			}
+		},
+		month: {
+			past: {
+				one: "{0} tháng trước",
+				other: "{0} tháng trước"
+			},
+			future: {
+				one: "trong {0} tháng",
+				other: "trong {0} tháng"
+			}
+		},
+		week: {
+			past: {
+				one: "{0} tuần trước",
+				other: "{0} tuần trước"
+			},
+			future: {
+				one: "trong {0} tuần",
+				other: "trong {0} tuần"
+			}
+		},
+		day: {
+			past: {
+				one: "{0} ngày trước",
+				other: "{0} ngày trước"
+			},
+			future: {
+				one: "trong {0} ngày",
+				other: "trong {0} ngày"
+			}
+		},
+		hour: {
+			past: {
+				one: "{0} tiếng trước",
+				other: "{0} tiếng trước"
+			},
+			future: {
+				one: "trong {0} tiếng",
+				other: "trong {0} tiếng"
+			}
+		},
+		minute: {
+			past: {
+				one: "{0} phút trước",
+				other: "{0} phút trước"
+			},
+			future: {
+				one: "trong {0} phút",
+				other: "trong {0} phút"
+			}
+		},
+		second: {
+			past: {
+				one: "{0} giây trước",
+				other: "{0} giây trước"
+			},
+			future: {
+				one: "trong {0} giây",
+				other: "trong {0} giây"
+			}
+		}
+	},
+	/* 'zero', 'one', 'two', 'few', 'many', 'other' */
+	plural: n => 1 == n ? 'one' : 'other'
+}
