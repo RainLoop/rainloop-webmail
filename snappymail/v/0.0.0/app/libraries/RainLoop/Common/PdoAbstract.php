52c95d4d0cb568d3a658ce75a9b5fef0dfdf7fd9
the-djmaze <>
2022-09-24 10:56:27 +0200

    Simplify DB schema

--- snappymail/v/0.0.0/app/libraries/RainLoop/Common/PdoAbstract.php
+++ snappymail/v/0.0.0/app/libraries/RainLoop/Common/PdoAbstract.php
@@ -310,18 +310,16 @@ abstract class PdoAbstract
 			{
 				case 'mysql':
 					$aQ[] = 'CREATE TABLE IF NOT EXISTS rainloop_system (
-id bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-sys_name varchar(50) NOT NULL,
+sys_name varchar(64) NOT NULL,
 value_int int UNSIGNED NOT NULL DEFAULT 0,
-PRIMARY KEY(id),
-UNIQUE INDEX sys_name_rainloop_system_index (sys_name)
-) ENGINE=INNODB CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;';
+PRIMARY KEY (sys_name)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;';
 					$aQ[] = 'CREATE TABLE IF NOT EXISTS rainloop_users (
 id_user int UNSIGNED NOT NULL AUTO_INCREMENT,
-rl_email varchar(254) NOT NULL DEFAULT \'\',
-PRIMARY KEY(id_user),
-INDEX rl_email_rainloop_users_index (rl_email)
-) ENGINE=INNODB;';
+rl_email varchar(254) NOT NULL,
+PRIMARY KEY (id_user),
+UNIQUE KEY ui_rainloop_users_email (rl_email)
+);';
 					break;
 
 				case 'pgsql':
@@ -343,7 +341,7 @@ rl_email varchar(254) NOT NULL DEFAULT \'\'
 sys_name text NOT NULL,
 value_int integer NOT NULL DEFAULT 0
 );';
-					$aQ[] = 'CREATE INDEX sys_name_rainloop_system_index ON rainloop_system (sys_name);';
+					$aQ[] = 'CREATE UNIQUE INDEX ui_rainloop_system_sys_name ON rainloop_system (sys_name);';
 					$aQ[] = 'CREATE TABLE rainloop_users (
 id_user integer NOT NULL PRIMARY KEY,
 rl_email text NOT NULL DEFAULT \'\'
