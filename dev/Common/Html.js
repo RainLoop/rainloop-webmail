50dffed89597f5431e08a5845cf0e3a3bb139f63
the-djmaze <>
2022-10-13 22:39:13 +0200

    Strip tracking of zalando.nl

--- dev/Common/Html.js
+++ dev/Common/Html.js
@@ -37,6 +37,7 @@ const
 	stripTracking = url => {
 		url = url
 			.replace(/tracking\.(printabout\.nl[^?]+)\?.*/i, (...m) => m[1])
+			.replace(/(zalando\.nl[^?]+)\?.*/i, (...m) => m[1])
 			.replace(/^.+(awstrack\.me|redditmail\.com)\/.+(https:%2F%2F[^/]+).*/i, (...m) => decodeURIComponent(m[2]))
 			.replace(/^.+(www\.google|safelinks\.protection\.outlook\.com).+$/i, () => urlGetParam(url, 'url'))
 			.replace(/^.+delivery-status\.com.+$/i, () => urlGetParam(url, 'fb'))
