c8ee67c3ba453e49e6ac1b23ecef393ac038f229
the-djmaze <>
2022-11-07 20:49:02 +0100

    Bugfix: setting snappymail-no-embed failed

--- integrations/nextcloud/snappymail/lib/Controller/FetchController.php
+++ integrations/nextcloud/snappymail/lib/Controller/FetchController.php
@@ -51,7 +51,7 @@ class FetchController extends Controller {
 					isset($_POST['snappymail-autologin']) ? '1' === $_POST['snappymail-autologin'] : false);
 				$this->config->setAppValue('snappymail', 'snappymail-autologin-with-email',
 					isset($_POST['snappymail-autologin']) ? '2' === $_POST['snappymail-autologin'] : false);
-//				$this->config->setAppValue('snappymail', 'snappymail-no-embed', isset($_POST['snappymail-no-embed']));
+				$this->config->setAppValue('snappymail', 'snappymail-no-embed', isset($_POST['snappymail-no-embed']));
 			} else {
 				return new JSONResponse([
 					'status' => 'error',
