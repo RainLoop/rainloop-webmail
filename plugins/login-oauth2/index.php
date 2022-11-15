901e45d67241ab41d852ef74a8fd4cdbaefa2f9b
the-djmaze <>
2022-11-15 11:56:30 +0100

    And a custom set_include_path() for #450

--- plugins/login-oauth2/index.php
+++ plugins/login-oauth2/index.php
@@ -33,6 +33,7 @@ class LoginOAuth2Plugin extends \RainLoop\Plugins\AbstractPlugin
 //		$this->addHook('sieve.after-login', array($this, $oSieveClient, $oSettings));
 		$this->addHook('filter.account', 'filterAccount');
 
+//		set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
 		spl_autoload_register(function($classname){
 			if (str_starts_with($classname, 'OAuth2\\')) {
 				include_once __DIR__ . strtr("\\{$sClassName}", '\\', DIRECTORY_SEPARATOR) . '.php';
