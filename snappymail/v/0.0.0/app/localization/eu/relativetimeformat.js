b29055a825452621d685641ad4539fd26aacf7d3
Olloki <olloki_unai@hotmail.com>
2022-11-24 12:51:29 +0100

    Basque relativetimeformat.js
    Basque translation on relativetimeformat.js

---
+++ snappymail/v/0.0.0/app/localization/eu/relativetimeformat.js
@@ -0,0 +1,75 @@
+{
+	long: {
+		year: {
+			past: {
+				one: "duela urtebete",
+				other: "duela {0} urte"
+			},
+			future: {
+				one: "urtebete barru",
+				other: "{0} urte barru"
+			}
+		},
+		month: {
+			past: {
+				one: "duela hilabete",
+				other: "duela {0} hilabete"
+			},
+			future: {
+				one: "hilabete barru",
+				other: "{0} hilabete barru"
+			}
+		},
+		week: {
+			past: {
+				one: "duela astebete",
+				other: "duela {0} aste"
+			},
+			future: {
+				one: "astebete barru",
+				other: "{0} aste barru"
+			}
+		},
+		day: {
+			past: {
+				one: "duela egun bat",
+				other: "duela {0} egun"
+			},
+			future: {
+				one: "egun bat barru",
+				other: "{0} egun barru"
+			}
+		},
+		hour: {
+			past: {
+				one: "duela ordubete",
+				other: "duela {0} ordu"
+			},
+			future: {
+				one: "ordubete barru",
+				other: "{0} ordu barru"
+			}
+		},
+		minute: {
+			past: {
+				one: "duela minutu {0}",
+				other: "duela {0} minutu"
+			},
+			future: {
+				one: "minutu {0} barru",
+				other: "{0} minutu barru"
+			}
+		},
+		second: {
+			past: {
+				one: "duela segundo {0}",
+				other: "duela {0} segundo"
+			},
+			future: {
+				one: "segundo {0} barru",
+				other: "{0} segundo barru"
+			}
+		}
+	},
+	plural: n => 1 == n ? 'one' : 'other'
+}
