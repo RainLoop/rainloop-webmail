3948b119989568bfe1c9ae219914e1321879844a
the-djmaze <>
2022-11-24 13:09:47 +0100

    Support cPanel #697

--- _include.php
+++ _include.php
@@ -29,6 +29,15 @@
  */
 //define('APP_DATA_FOLDER_PATH', dirname(__DIR__) . '/snappymail-data/');
 //define('APP_DATA_FOLDER_PATH', '/var/external-snappymail-data-folder/');
+if (isset($_ENV['CPANEL']) && isset($_ENV['TMPDIR'])) {
+	define('APP_DATA_FOLDER_PATH', $_ENV['TMPDIR'] . '/snappymail/');
+/*
+	$_ENV['USER'] => <username>
+	$_ENV['HOME'] => /home/<username>
+	$_ENV['TMPDIR'] => /home/<username>/tmp
+	$_ENV['REAL_DBOWNER'] => <username>
+*/
+}
 
 /**
  * Additional configuration file name
