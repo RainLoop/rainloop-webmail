73faa027953d0c067fbed4db6f2a916f8195523b
cm-schl <63400209+cm-schl@users.noreply.github.com>
2022-11-25 11:41:31 +0100

    better description of config parameters

--- plugins/ldap-mail-accounts/index.php
+++ plugins/ldap-mail-accounts/index.php
@@ -15,10 +15,10 @@ class LdapMailAccountsPlugin extends AbstractPlugin
 		VERSION  = '1.0',
 		AUTHOR   = 'cm-schl',
 		URL      = 'https://github.com/cm-sch',
-		RELEASE  = '2022-11-11',
+		RELEASE  = '2022-11-25',
 		REQUIRED = '2.20.0',
 		CATEGORY = 'Accounts',
-		DESCRIPTION = 'Adds functionality to import mail accounts from LDAP. Basing on the work of FWest98 (https://github.com/FWest98).';
+		DESCRIPTION = 'Add additional mail accounts the SnappyMail user has access to by a LDAP query. Basing on the work of FWest98 (https://github.com/FWest98).';
 
 	public function __construct()
 	{
@@ -59,29 +59,30 @@ class LdapMailAccountsPlugin extends AbstractPlugin
 				->SetDefaultValue([2, 3]),
 
 			Property::NewInstance(LdapConfig::CONFIG_BIND_USER)
-				->SetLabel("Bind User DN")
+				->SetLabel("LDAP Username")
 				->SetDescription("The user to use for binding to the LDAP server. Should be a DN or RDN. Leave empty for anonymous bind.")
 				->SetType(PluginPropertyType::STRING),
 
 			Property::NewInstance(LdapConfig::CONFIG_BIND_PASSWORD)
-				->SetLabel("Bind User Password")
+				->SetLabel("LDAP Password")
 				->SetDescription("Leave empty for anonymous bind.")
 				->SetType(PluginPropertyType::PASSWORD),
 
 			Property::NewInstance(LdapConfig::CONFIG_OBJECTCLASS)
 				->SetLabel("Object class")
 				->SetType(PluginPropertyType::STRING)
+				->SetDescription("The object class to use when searching for additional mail accounts of the logged in SnappyMail user")
 				->SetDefaultValue("user"),
 
 			Property::NewInstance(LdapConfig::CONFIG_BASE)
-				->SetLabel("User base DN")
+				->SetLabel("Base DN")
 				->SetType(PluginPropertyType::STRING)
-				->SetDescription("The base DN to search in for users."),
+				->SetDescription("The base DN to search in for additional mail accounts of the logged in SnappyMail user"),
 
 			Property::NewInstance(LdapConfig::CONFIG_FIELD_SEARCH)
 				->SetLabel("Search field")
 				->SetType(PluginPropertyType::STRING)
-				->SetDescription("The name of the ldap attribute that has to contain the set 'LDAP search string'.")
+				->SetDescription("The name of the ldap attribute that has to contain the here defined 'LDAP search string'.")
 				->SetDefaultValue("member"),				
 
 			Property::NewInstance(LdapConfig::CONFIG_SEARCH_STRING)
@@ -89,20 +90,23 @@ class LdapMailAccountsPlugin extends AbstractPlugin
 				->SetType(PluginPropertyType::STRING)
 				->SetDescription("The search string used to find ldap objects of mail accounts the user has access to.
 					\nPossible placeholers:\n#USERNAME# - replaced with the username of the actual SnappyMail user
-					\n#BASE_DN# - replaced with the value inside the field 'User base DN'."),
+					\n#BASE_DN# - replaced with the value inside the field 'User base DN'.")
+				->SetDefaultValue("uid=#USERNAME#"),	
 
 			Property::NewInstance(LdapConfig::CONFIG_FIELD_USERNAME)
 				->SetLabel("Username field of additional account")
 				->SetType(PluginPropertyType::STRING)
 				->SetDescription("The field containing the username of the found additional mail account. 
-					\nThis username gets used by SnappyMail to login to the additional mail account.")
+					\nThis username gets used by SnappyMail to login to the additional mail account.
+					\nIf this field contains an email address, only the local-part before the @ is used.")
 				->SetDefaultValue("uid"),
 
 			Property::NewInstance(LdapConfig::CONFIG_FIELD_MAIL_DOMAIN)
 				->SetLabel("Domain name field of additional account")
 				->SetType(PluginPropertyType::STRING)
 				->SetDescription("The field containing the domain name of the found additional mail account. 
-					\nThis domain gets looked up by SnappyMail to choose the right connection parameters at logging in to the additional mail account.")
+					\nThis domain gets looked up by SnappyMail to choose the right connection parameters at logging in to the additional mail account.
+					\nIf this field contains an email address, only the domain-part after the @ is used.")
 				->SetDefaultValue("mail"),				
 
 			Property::NewInstance(LdapConfig::CONFIG_FIELD_NAME)
