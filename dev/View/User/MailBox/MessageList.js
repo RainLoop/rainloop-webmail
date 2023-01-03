0e5ae38c58516276f2af0deee5746df7ff568717
the-djmaze <>
2023-01-03 14:15:09 +0100

    More changes for #815

--- dev/View/User/MailBox/MessageList.js
+++ dev/View/User/MailBox/MessageList.js
@@ -818,8 +818,12 @@ export class MailMessageList extends AbstractViewRight {
 		showScreenPopup(AdvancedSearchPopupView, [MessagelistUserStore.mainSearch()]);
 	}
 
-	groupSearchClick(group) {
+	groupSearch(group) {
 		group.search && MessagelistUserStore.mainSearch(group.search);
+	}
+
+	groupCheck(group) {
+		group.messages.forEach(message => message.checked(!message.checked()));
 	}
 
 	quotaTooltip() {
