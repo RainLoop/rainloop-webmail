f66284128165be4db74425b6ee628e50e6c9887b
the-djmaze <>
2023-02-22 12:18:49 +0100

    Resolve #987

--- plugins/nextcloud/index.php
+++ plugins/nextcloud/index.php
@@ -4,8 +4,8 @@ class NextcloudPlugin extends \RainLoop\Plugins\AbstractPlugin
 {
 	const
 		NAME = 'Nextcloud',
-		VERSION = '2.18',
-		RELEASE  = '2023-02-20',
+		VERSION = '2.19',
+		RELEASE  = '2023-02-22',
 		CATEGORY = 'Integrations',
 		DESCRIPTION = 'Integrate with Nextcloud v20+',
 		REQUIRED = '2.25.1';
@@ -202,13 +202,15 @@ class NextcloudPlugin extends \RainLoop\Plugins\AbstractPlugin
 				}
 				if ($bSave) {
 					$oActions = \RainLoop\Api::Actions();
-					$oAccount = $oActions->getAccountFromToken();
-					$this->setContactsSyncData($oAccount, array(
-						'Mode' => $aResult['ContactsSync']['Mode'],
-						'User' => $aResult['ContactsSync']['User'],
-						'Password' => $aResult['ContactsSync']['Password'],
-						'Url' => $aResult['ContactsSync']['Url']
-					));
+					$oActions->setContactsSyncData(
+						$oActions->getAccountFromToken(),
+						array(
+							'Mode' => $aResult['ContactsSync']['Mode'],
+							'User' => $aResult['ContactsSync']['User'],
+							'Password' => $aResult['ContactsSync']['Password'],
+							'Url' => $aResult['ContactsSync']['Url']
+						)
+					);
 				}
 			}
 		}
