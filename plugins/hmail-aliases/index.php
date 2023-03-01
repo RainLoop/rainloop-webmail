1cf32d6c90418824bef609d45519e5c72b626e2d
Timothy Sturm <timothysturm6@gmail.com>
2023-03-01 17:44:31 -0600

    Create Rainloop identities from hMail
    Use com api instead of direct dp access

--- plugins/hmail-aliases/index.php
+++ plugins/hmail-aliases/index.php
@@ -9,11 +9,12 @@ class HmailAliasesPlugin extends \RainLoop\Plugins\AbstractPlugin
 
     public function Supported()
     {
-        if (!class_exists('mysqli')) {
-            return 'The PHP exention mysqli must be installed to use this plugin';
-        }
+		if (!class_exists('COM'))
+		{
+			return 'The PHP extension COM must be installed to use this plugin';
+		}
 
-        return '';
+		return '';
     }
 
     /**
@@ -22,19 +23,104 @@ class HmailAliasesPlugin extends \RainLoop\Plugins\AbstractPlugin
     */
     public function loginPostLoginProvide(\RainLoop\Model\Account &$oAccount)
     {
-        $dbservername = \trim($this->Config()->Get('plugin', 'hamilserver', ''));
-        $dbusername = \trim($this->Config()->Get('plugin', 'hmaildbusername', ''));
-        $dbpassword = \trim($this->Config()->Get('plugin', 'hmaildbpassword', ''));
-        $dbname = \trim($this->Config()->Get('plugin', 'hmaildbtable', ''));
-        $dbport = \trim($this->Config()->Get('plugin', 'hmaildbport', ''));
-        $datadir = \trim($this->Config()->Get('plugin', 'rainloopDatalocation', ''));
+		$oProvider = new HmailserverChangePasswordDriver();
+		$oLogger($this->Manager()->Actions()->Logger());
+		$sLogin = (string) $this->Config()->Get('plugin', 'login', '');
+		$sPassword = (string) $this->Config()->Get('plugin', 'password', '');
+		$bResult = false;
 
-        if ($datadir != ""){
-          $userpath = $datadir.'data/_data_/_default_/storage/cfg/'.substr($oAccount->Email(), 0, 2).'/'.$oAccount->Email().'/identities';
-        } else {
-          $userpath = APP_INDEX_ROOT_PATH.'_data_/_default_/storage/cfg/'.substr($oAccount->Email(), 0, 2).'/'.$oAccount->Email().'/identities';
-        }
+		try
+		{
+			$oHmailApp = new COM("hMailServer.Application");
+			$oHmailApp->Connect();
 
+			if ($oHmailApp->Authenticate($sLogin, $sPassword))
+			{
+				$sEmail = $oHmailAccount->Email();
+				$sDomain = \MailSo\Base\Utils::GetDomainFromEmail($sEmail);
+
+				$oHmailDomain = $oHmailApp->Domains->ItemByName($sDomain);
+				if ($oHmailDomain)
+				{
+					$oHmailAccount = $oHmailDomain->Accounts->ItemByAddress($sEmail);
+					if ($oHmailAccount)
+					{
+                        // Get account details for alias
+                        
+                        $firstName = $oHmailAccount->PersonFirstName;
+                        $lastName = $oHmailAccount->PersonLastName;
+                        if ($firstName == "" && $lastName == "") {
+                            $name = "";
+                        } else {
+                            $name = $firstName." ".$lastName;
+                        }
+                        
+                        // ========vvv========= Update Rainloop Idnetity ========vvv=========
+
+                        $datadir = \trim($this->Config()->Get('plugin', 'rainloopDatalocation', ''));
+                        if ($datadir != ""){
+                            $userpath = $datadir.'data/_data_/_default_/storage/cfg/'.substr($oAccount->Email(), 0, 2).'/'.$oAccount->Email().'/identities';
+                        } else {
+                            $userpath = APP_INDEX_ROOT_PATH.'_data_/_default_/storage/cfg/'.substr($oAccount->Email(), 0, 2).'/'.$oAccount->Email().'/identities';
+                        }
+        
+                        $newidentitiesobj = array();
+                        
+                        //Get existing settings. If not a alias created by hmail. Transfer settings to the new array.
+                        $identities = file_get_contents($userpath, true);
+                        if ($identities != "") {
+                            $identities = json_decode($identities, true);
+                            error_log(print_r($identities, true));
+                            foreach ($identities as $row) {
+                                if (strpos($row['Id'], 'HMAIL') === false) {
+                                    array_push($newidentitiesobj, $row);
+                                }
+                            }
+                        }
+                        
+                        $obj = array();
+                        $obj['Id'] = "HMAIL".base64_encode($sEmail);
+                        $obj['Email'] = $sEmail;
+                        $obj['Name'] = $name;
+                        $obj['ReplyTo'] = "";
+                        $obj['Bcc'] = "";
+                        $obj['Signature'] = "";
+                        $obj['SignatureInsertBefore'] = false;
+                        array_push($newidentitiesobj, $obj);
+                        
+                        file_put_contents($userpath, json_encode($newidentitiesobj));
+                        // ========^^^========= Update Rainloop Idnetity ========^^^=========
+
+						$bResult = true;
+					}
+					else
+					{
+						$this->oLogger->Write('HMAILSERVER: Unknown account ('.$sEmail.')', \MailSo\Log\Enumerations\Type::ERROR);
+					}
+				}
+				else
+				{
+					$this->oLogger->Write('HMAILSERVER: Unknown domain ('.$sDomain.')', \MailSo\Log\Enumerations\Type::ERROR);
+				}
+			}
+			else
+			{
+				$this->oLogger->Write('HMAILSERVER: Auth error', \MailSo\Log\Enumerations\Type::ERROR);
+			}
+		}
+		catch (\Exception $oException)
+		{
+			if ($this->oLogger)
+			{
+				$this->oLogger->WriteException($oException);
+			}
+		}
+
+		return $bResult;
+        
+        //==========================================================================
+
+        
         $hmailconn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname, $dbport);
 
         // Check connection
@@ -94,17 +180,11 @@ class HmailAliasesPlugin extends \RainLoop\Plugins\AbstractPlugin
     public function configMapping()
     {
         return array(
-          \RainLoop\Plugins\Property::NewInstance('hamilserver')->SetLabel('db-host')
-            ->SetDefaultValue('localhost'),
-          \RainLoop\Plugins\Property::NewInstance('hmaildbusername')->SetLabel('db-username')
-            ->SetDefaultValue(''),
-          \RainLoop\Plugins\Property::NewInstance('hmaildbpassword')->SetLabel('db-password')
-            ->SetType(\RainLoop\Enumerations\PluginPropertyType::PASSWORD),
-          \RainLoop\Plugins\Property::NewInstance('hmaildbtable')->SetLabel('db-table')
-            ->SetDefaultValue('mailserver'),
-          \RainLoop\Plugins\Property::NewInstance('hmaildbport')->SetLabel('db-port')
-            ->SetDefaultValue('3306')
-            ->SetDescription('Connect to mysql hmailserver. The user must have rights to read "hm_aliases" table.'),
+		  \RainLoop\Plugins\Property::NewInstance('login')->SetLabel('HmailServer Admin Login')
+			->SetDefaultValue('Administrator'),
+		  \RainLoop\Plugins\Property::NewInstance('password')->SetLabel('HmailServer Admin Password')
+			->SetType(\RainLoop\Enumerations\PluginPropertyType::PASSWORD)
+			->SetDefaultValue(''),
           \RainLoop\Plugins\Property::NewInstance('rainloopDatalocation')->SetLabel('Data folder location')
             ->SetDefaultValue('')
             ->SetDescription('Incase of custom data directory location. Eg. nextcloud/owncloud version (Leave blank for default)')
