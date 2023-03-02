3a2b30bc673196330a4db5d26524c87506621ac2
Timothy Sturm <timothysturm6@gmail.com>
2023-03-01 19:44:11 -0600

    remove old code
    oops

--- plugins/hmail-aliases/index.php
+++ plugins/hmail-aliases/index.php
@@ -23,155 +23,100 @@ class HmailAliasesPlugin extends \RainLoop\Plugins\AbstractPlugin
     */
     public function loginPostLoginProvide(\RainLoop\Model\Account &$oAccount)
     {
-		$oProvider = new HmailserverChangePasswordDriver();
-		$oLogger($this->Manager()->Actions()->Logger());
-		$sLogin = (string) $this->Config()->Get('plugin', 'login', '');
-		$sPassword = (string) $this->Config()->Get('plugin', 'password', '');
-		$bResult = false;
+	$oProvider = new HmailserverChangePasswordDriver();
+	$oLogger($this->Manager()->Actions()->Logger());
+	$sLogin = (string) $this->Config()->Get('plugin', 'login', '');
+	$sPassword = (string) $this->Config()->Get('plugin', 'password', '');
+	$bResult = false;
 
-		try
+	try
+	{
+		$oHmailApp = new COM("hMailServer.Application");
+		$oHmailApp->Connect();
+
+		if ($oHmailApp->Authenticate($sLogin, $sPassword))
 		{
-			$oHmailApp = new COM("hMailServer.Application");
-			$oHmailApp->Connect();
+			$sEmail = $oHmailAccount->Email();
+			$sDomain = \MailSo\Base\Utils::GetDomainFromEmail($sEmail);
 
-			if ($oHmailApp->Authenticate($sLogin, $sPassword))
+			$oHmailDomain = $oHmailApp->Domains->ItemByName($sDomain);
+			if ($oHmailDomain)
 			{
-				$sEmail = $oHmailAccount->Email();
-				$sDomain = \MailSo\Base\Utils::GetDomainFromEmail($sEmail);
-
-				$oHmailDomain = $oHmailApp->Domains->ItemByName($sDomain);
-				if ($oHmailDomain)
+				$oHmailAccount = $oHmailDomain->Accounts->ItemByAddress($sEmail);
+				if ($oHmailAccount)
 				{
-					$oHmailAccount = $oHmailDomain->Accounts->ItemByAddress($sEmail);
-					if ($oHmailAccount)
-					{
-                        // Get account details for alias
-                        
-                        $firstName = $oHmailAccount->PersonFirstName;
-                        $lastName = $oHmailAccount->PersonLastName;
-                        if ($firstName == "" && $lastName == "") {
-                            $name = "";
-                        } else {
-                            $name = $firstName." ".$lastName;
-                        }
-                        
-                        // ========vvv========= Update Rainloop Idnetity ========vvv=========
+		// Get account details for alias
 
-                        $datadir = \trim($this->Config()->Get('plugin', 'rainloopDatalocation', ''));
-                        if ($datadir != ""){
-                            $userpath = $datadir.'data/_data_/_default_/storage/cfg/'.substr($oAccount->Email(), 0, 2).'/'.$oAccount->Email().'/identities';
-                        } else {
-                            $userpath = APP_INDEX_ROOT_PATH.'_data_/_default_/storage/cfg/'.substr($oAccount->Email(), 0, 2).'/'.$oAccount->Email().'/identities';
-                        }
-        
-                        $newidentitiesobj = array();
-                        
-                        //Get existing settings. If not a alias created by hmail. Transfer settings to the new array.
-                        $identities = file_get_contents($userpath, true);
-                        if ($identities != "") {
-                            $identities = json_decode($identities, true);
-                            error_log(print_r($identities, true));
-                            foreach ($identities as $row) {
-                                if (strpos($row['Id'], 'HMAIL') === false) {
-                                    array_push($newidentitiesobj, $row);
-                                }
-                            }
-                        }
-                        
-                        $obj = array();
-                        $obj['Id'] = "HMAIL".base64_encode($sEmail);
-                        $obj['Email'] = $sEmail;
-                        $obj['Name'] = $name;
-                        $obj['ReplyTo'] = "";
-                        $obj['Bcc'] = "";
-                        $obj['Signature'] = "";
-                        $obj['SignatureInsertBefore'] = false;
-                        array_push($newidentitiesobj, $obj);
-                        
-                        file_put_contents($userpath, json_encode($newidentitiesobj));
-                        // ========^^^========= Update Rainloop Idnetity ========^^^=========
+		$firstName = $oHmailAccount->PersonFirstName;
+		$lastName = $oHmailAccount->PersonLastName;
+		if ($firstName == "" && $lastName == "") {
+		    $name = "";
+		} else {
+		    $name = $firstName." ".$lastName;
+		}
 
-						$bResult = true;
-					}
-					else
-					{
-						$this->oLogger->Write('HMAILSERVER: Unknown account ('.$sEmail.')', \MailSo\Log\Enumerations\Type::ERROR);
-					}
+		// ========vvv========= Update Rainloop Idnetity ========vvv=========
+
+		$datadir = \trim($this->Config()->Get('plugin', 'rainloopDatalocation', ''));
+		if ($datadir != ""){
+		    $userpath = $datadir.'data/_data_/_default_/storage/cfg/'.substr($oAccount->Email(), 0, 2).'/'.$oAccount->Email().'/identities';
+		} else {
+		    $userpath = APP_INDEX_ROOT_PATH.'_data_/_default_/storage/cfg/'.substr($oAccount->Email(), 0, 2).'/'.$oAccount->Email().'/identities';
+		}
+
+		$newidentitiesobj = array();
+
+		//Get existing settings. If not a alias created by hmail. Transfer settings to the new array.
+		$identities = file_get_contents($userpath, true);
+		if ($identities != "") {
+		    $identities = json_decode($identities, true);
+		    error_log(print_r($identities, true));
+		    foreach ($identities as $row) {
+			if (strpos($row['Id'], 'HMAIL') === false) {
+			    array_push($newidentitiesobj, $row);
+			}
+		    }
+		}
+
+		$obj = array();
+		$obj['Id'] = "HMAIL".base64_encode($sEmail);
+		$obj['Email'] = $sEmail;
+		$obj['Name'] = $name;
+		$obj['ReplyTo'] = "";
+		$obj['Bcc'] = "";
+		$obj['Signature'] = "";
+		$obj['SignatureInsertBefore'] = false;
+		array_push($newidentitiesobj, $obj);
+
+		file_put_contents($userpath, json_encode($newidentitiesobj));
+		// ========^^^========= Update Rainloop Idnetity ========^^^=========
+
+					$bResult = true;
 				}
 				else
 				{
-					$this->oLogger->Write('HMAILSERVER: Unknown domain ('.$sDomain.')', \MailSo\Log\Enumerations\Type::ERROR);
+					$this->oLogger->Write('HMAILSERVER: Unknown account ('.$sEmail.')', \MailSo\Log\Enumerations\Type::ERROR);
 				}
 			}
 			else
 			{
-				$this->oLogger->Write('HMAILSERVER: Auth error', \MailSo\Log\Enumerations\Type::ERROR);
+				$this->oLogger->Write('HMAILSERVER: Unknown domain ('.$sDomain.')', \MailSo\Log\Enumerations\Type::ERROR);
 			}
 		}
-		catch (\Exception $oException)
+		else
 		{
-			if ($this->oLogger)
-			{
-				$this->oLogger->WriteException($oException);
-			}
+			$this->oLogger->Write('HMAILSERVER: Auth error', \MailSo\Log\Enumerations\Type::ERROR);
 		}
+	}
+	catch (\Exception $oException)
+	{
+		if ($this->oLogger)
+		{
+			$this->oLogger->WriteException($oException);
+		}
+	}
 
-		return $bResult;
-        
-        //==========================================================================
-
-        
-        $hmailconn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname, $dbport);
-
-        // Check connection
-        if (!$hmailconn) {
-            echo "Hmail-aliases: connection to db failed";
-            return;
-        }
-        //Get aliases
-        $result = $hmailconn->query("SELECT * FROM " . $dbname . ".hm_aliases WHERE aliasvalue='".$oAccount->Email()."'");
-
-        if ($result->num_rows > 0) {
-            $newidentitiesobj = array();
-
-            //Get user account
-            $result2 = $hmailconn->query("SELECT * FROM " . $dbname . ".hm_accounts WHERE accountaddress='".$oAccount->Email()."'");
-            $result2 = $result2->fetch_assoc();
-            $firstname = $result2['accountpersonfirstname'];
-            $lastname = $result2['accountpersonlastname'];
-
-            if ($firstname == "" && $lastname == "") {
-                $name = "";
-            } else {
-                $name = $firstname." ".$lastname;
-            }
-
-            //Get existing settings. If not a alias created by hmail. Transfer settings to the new array.
-            $identities = file_get_contents($userpath, true);
-            if ($identities != "") {
-                $identities = json_decode($identities, true);
-                error_log(print_r($identities, true));
-                foreach ($identities as $row) {
-                    if (strpos($row['Id'], 'HMAIL') === false) {
-                        array_push($newidentitiesobj, $row);
-                    }
-                }
-            }
-
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-                $obj = array();
-                $obj['Id'] = "HMAIL".base64_encode($row["aliasname"]);
-                $obj['Email'] = $row["aliasname"];
-                $obj['Name'] = $name;
-                $obj['ReplyTo'] = "";
-                $obj['Bcc'] = "";
-                $obj['Signature'] = "";
-                $obj['SignatureInsertBefore'] = false;
-                array_push($newidentitiesobj, $obj);
-            }
-            file_put_contents($userpath, json_encode($newidentitiesobj));
-        }
+	return $bResult;
     }
 
     /**
