775faa9d7969ce7f3c2709f3b40ada8130c4e31c
the-djmaze <>
2023-10-01 11:18:37 +0200

    #1243 version bump

--- plugins/ldap-contacts-suggestions/index.php
+++ plugins/ldap-contacts-suggestions/index.php
@@ -4,8 +4,8 @@ class LdapContactsSuggestionsPlugin extends \RainLoop\Plugins\AbstractPlugin
 {
 	const
 		NAME     = 'Contacts suggestions (LDAP)',
-		VERSION  = '2.12',
-		RELEASE  = '2023-01-31',
+		VERSION  = '2.13',
+		RELEASE  = '2023-10-01',
 		REQUIRED = '2.23.0',
 		CATEGORY = 'Contacts',
 		DESCRIPTION = 'Get contacts suggestions from LDAP.';
