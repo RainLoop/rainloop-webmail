611c5f2631dd06f7b047498116b7cdce11bcbae9
the-djmaze <>
2023-10-02 14:46:26 +0200

    Resolve #1250

--- snappymail/v/0.0.0/setup.php
+++ snappymail/v/0.0.0/setup.php
@@ -34,6 +34,7 @@ $aRequirements = array(
 	'mbstring' => extension_loaded('mbstring'),
 	'Zlib'     => extension_loaded('zlib'),
 	// enabled by default:
+	'ctype'    => extension_loaded('ctype'),
 	'json'     => function_exists('json_decode'),
 	'libxml'   => function_exists('libxml_use_internal_errors'),
 	'dom'      => class_exists('DOMDocument')
