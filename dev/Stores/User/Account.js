1a5ff4772daaeebe6c5a1c9647f5fa769487454d
the-djmaze <>
2023-10-31 01:36:41 +0100

    Drop unused AccountUserStore.signature Drop debounce from AccountUserStore.loading

--- dev/Stores/User/Account.js
+++ dev/Stores/User/Account.js
@@ -2,11 +2,9 @@ import { addObservablesTo, koArrayWithDestroy } from 'External/ko';
 
 export const AccountUserStore = koArrayWithDestroy();
 
-AccountUserStore.loading = ko.observable(false).extend({ debounce: 100 });
-
 AccountUserStore.getEmailAddresses = () => AccountUserStore.map(item => item.email);
 
 addObservablesTo(AccountUserStore, {
 	email: '',
-	signature: ''
+	loading: false
 });
