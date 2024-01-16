8f9c19936049574eed365c85726ff655c1e21109
the-djmaze <>
2024-01-16 15:49:15 +0100

    Bump Nextcloud extension version

--- plugins/nextcloud/index.php
+++ plugins/nextcloud/index.php
@@ -4,8 +4,8 @@ class NextcloudPlugin extends \RainLoop\Plugins\AbstractPlugin
 {
 	const
 		NAME = 'Nextcloud',
-		VERSION = '2.28',
-		RELEASE  = '2023-12-17',
+		VERSION = '2.29',
+		RELEASE  = '2024-01-12',
 		CATEGORY = 'Integrations',
 		DESCRIPTION = 'Integrate with Nextcloud v20+',
 		REQUIRED = '2.27.0';
